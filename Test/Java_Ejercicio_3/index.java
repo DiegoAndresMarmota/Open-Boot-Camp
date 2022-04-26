0fc835a7069c261d16a196536bf38fd5efd0ee63
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-04-26 17:46:42 -0400

    Java_Ejercicio_3

---
+++ Test/Java_Ejercicio_3/index.java
@@ -0,0 +1,28 @@
+/*Primera parte:
+Crear una función con tres parámetros que sean números que se suman entre sí.
+Llamar a la función en el main y darle valores.
+
+Segunda parte:
+Crear una clase coche.
+Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+Una función que incremente el número de puertas que tiene el coche.
+Crear un objeto miCoche en el main y añadirle una puerta.
+Mostrar el número de puertas que tiene el objeto.*/
+
+package com.company;
+public class Main {
+    public static void main(String[] args) {suma(1, 2, 3);
+        Coche Coche = new Coche();
+            miCoche.sumarPuertas();
+            miCoche.sumarPuertas();
+            System.out.println("Total de puertas:" + Coche.TotalPuertas);
+
+public static void suma(int a, int b, int c){
+        int resultado;
+        Total = a + b + c;
+        System.out.println(total);}}
+
+class Coche {
+        public int TotalPuertas = 4;
+        public void TotalPuertas(){
+        this.TotalPuertas++;}}
