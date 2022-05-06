53030f2c49a645bf73eccbf947864cf52aad3475
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-05-06 14:54:58 -0400

    Actualizado 14:55

--- JAVASCRIPT/16. Tareas/Ejercicio_tema_4.js
+++ JAVASCRIPT/16. Tareas/Ejercicio_tema_4.js
@@ -2,3 +2,8 @@
 
 let frase = "El perro de mi vecina es muy molesto"
 console.log(texto_largo.replace('vecina', 'vecino'))
+
+/*Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.*/
+
+let frase1 = "El perro digital de mi vecina es muy molestoso y se comio mi tarea para OpenBootCamp"
+console.log(frase1.toUpperCase())
