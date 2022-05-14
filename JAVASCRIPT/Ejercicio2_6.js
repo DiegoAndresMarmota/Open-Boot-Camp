91e7a83598dbeefb87e5230ad66659eed93bf0b7
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-05-14 18:23:50 -0400

    Create Ejercicio2_6.js

---
+++ JAVASCRIPT/Ejercicio2_6.js
@@ -0,0 +1,14 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const archivoLista = ["Diego", 36, true, new Date(1987, 10, 20), { 
+      titulo: "El amor en tiempos del cólera",
+      autor: "Gabriel Garcia Marquez",
+      fecha: new Date(1985, 10, 3),
+      url: "https://www.buscalibre.cl/libro-el-amor-en-los-tiempos-del-colera/9789871138135/p/1039459"
+    },
+];
